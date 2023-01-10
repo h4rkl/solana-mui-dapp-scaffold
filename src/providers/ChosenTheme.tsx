ceae6ebbec0375bd04f481e2993a1542e40e08b2
harkl <3zy3@protonmail.ch>
2023-01-11 08:55:55 +1300

    persist localstorage darkmode value

--- src/providers/ChosenTheme.tsx
+++ src/providers/ChosenTheme.tsx
@@ -1,23 +1,32 @@
-import React, { createContext, Dispatch, SetStateAction } from 'react'
-import useMediaQuery from '@mui/material/useMediaQuery'
-import { useLocalStorage } from '@caldwell619/react-hooks'
+import React, { createContext, Dispatch, SetStateAction } from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useLocalStorage } from "@caldwell619/react-hooks";
 
-type ThemeName = 'dark' | 'light'
+type ThemeName = "dark" | "light";
 
 interface IChosenTheme {
-  theme: ThemeName
-  setTheme: Dispatch<SetStateAction<ThemeName>>
+  theme: ThemeName;
+  setTheme: Dispatch<SetStateAction<ThemeName>>;
 }
 
 interface Props {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export const ChosenTheme = createContext<IChosenTheme>({} as IChosenTheme)
+export const ChosenTheme = createContext<IChosenTheme>({} as IChosenTheme);
 
-export const ChosenThemeProvider: React.FC<Props>  = ({ children }) => {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  const [theme, setTheme] = useLocalStorage<ThemeName>('theme', prefersDarkMode ? 'dark' : 'light', true)
+export const ChosenThemeProvider: React.FC<Props> = ({ children }) => {
+  let prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  if (window.localStorage.getItem("theme") === "light") prefersDarkMode = false;
+  const [theme, setTheme] = useLocalStorage<ThemeName>(
+    "theme",
+    prefersDarkMode ? "dark" : "light",
+    true
+  );
 
-  return <ChosenTheme.Provider value={{ theme, setTheme }}>{children}</ChosenTheme.Provider>
-}
+  return (
+    <ChosenTheme.Provider value={{ theme, setTheme }}>
+      {children}
+    </ChosenTheme.Provider>
+  );
+};
