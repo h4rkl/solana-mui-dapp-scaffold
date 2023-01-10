3143fc782643e036ecdf1020a311b7023bf31065
harkl <3zy3@protonmail.ch>
2023-01-11 10:35:13 +1300

    tidy wallet context

--- src/contexts/WalletContextProvider.tsx
+++ src/contexts/WalletContextProvider.tsx
@@ -1,76 +1,101 @@
-import { WalletAdapterNetwork, WalletError } from '@solana/wallet-adapter-base';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { AutoConnectProvider, useAutoConnect } from "./AutoConnectProvider";
+import { clusterApiUrl } from "@solana/web3.js";
 import {
-    PhantomWalletAdapter,
-    SolflareWalletAdapter,
-    SolletExtensionWalletAdapter,
-    SolletWalletAdapter,
-    TorusWalletAdapter,
-    // LedgerWalletAdapter,
-    // SlopeWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import { Cluster, clusterApiUrl } from '@solana/web3.js';
-import { FC, ReactNode, useCallback, useMemo } from 'react';
-import { AutoConnectProvider, useAutoConnect } from './AutoConnectProvider';
-import { notify } from "../utils/notifications";
-import { NetworkConfigurationProvider, useNetworkConfiguration } from './NetworkConfigurationProvider';
-import dynamic from "next/dynamic";
-
-const ReactUIWalletModalProviderDynamic = dynamic(
-  async () =>
-    (await import("@solana/wallet-adapter-react-ui")).WalletModalProvider,
-  { ssr: false }
-);
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  FC,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import {
+  NetworkConfigurationProvider,
+  useNetworkConfiguration,
+} from "./NetworkConfigurationProvider";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+  // LedgerWalletAdapter,
+  // SlopeWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { Snackbar } from "@mui/material";
+import { WalletAdapterNetwork, WalletError } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const { autoConnect } = useAutoConnect();
-    const { networkConfiguration } = useNetworkConfiguration();
-    const network = networkConfiguration as WalletAdapterNetwork;
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const { autoConnect } = useAutoConnect();
+  const { networkConfiguration } = useNetworkConfiguration();
+  const network = networkConfiguration as WalletAdapterNetwork;
+  const [snackMessage, setSnackMessage] = useState<string | null>(null);
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-    console.log(network);
+  useEffect(() => {
+    setSnackMessage("Hooray");
+  }, []);
 
-    const wallets = useMemo(
-        () => [
-            new PhantomWalletAdapter(),
-            new SolflareWalletAdapter(),
-            new SolletWalletAdapter({ network }),
-            new SolletExtensionWalletAdapter({ network }),
-            new TorusWalletAdapter(),
-            // new LedgerWalletAdapter(),
-            // new SlopeWalletAdapter(),
-        ],
-        [network]
+  console.log(network);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new SolletWalletAdapter({ network }),
+      new SolletExtensionWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+      // new LedgerWalletAdapter(),
+      // new SlopeWalletAdapter(),
+    ],
+    [network]
+  );
+
+  const handleClose = () => {
+    setSnackMessage(null);
+  };
+
+  const onError = useCallback((error: WalletError) => {
+    setSnackMessage(
+      error.message ? `${error.name}: ${error.message}` : error.name
     );
+    console.error(error);
+  }, []);
 
-    const onError = useCallback(
-        (error: WalletError) => {
-            notify({ type: 'error', message: error.message ? `${error.name}: ${error.message}` : error.name });
-            console.error(error);
-        },
-        []
-    );
-
-    return (
-        // TODO: updates needed for updating and referencing endpoint: wallet adapter rework
-        <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} onError={onError} autoConnect={autoConnect}>
-                <ReactUIWalletModalProviderDynamic>
-                    {children}
-                </ReactUIWalletModalProviderDynamic>
-			</WalletProvider>
-        </ConnectionProvider>
-    );
+  return (
+    // TODO: updates needed for updating and referencing endpoint: wallet adapter rework
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider
+        wallets={wallets}
+        onError={onError}
+        autoConnect={autoConnect}
+      >
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+        <Snackbar
+          open={!!snackMessage}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          message={snackMessage}
+        />
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 };
 
-export const ContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    return (
-        <>
-            <NetworkConfigurationProvider>
-                <AutoConnectProvider>
-                    <WalletContextProvider>{children}</WalletContextProvider>
-                </AutoConnectProvider>
-            </NetworkConfigurationProvider>
-        </>
-    );
+export const WalletContext: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <>
+      <NetworkConfigurationProvider>
+        <AutoConnectProvider>
+          <WalletContextProvider>{children}</WalletContextProvider>
+        </AutoConnectProvider>
+      </NetworkConfigurationProvider>
+    </>
+  );
 };
