4ab9047945168ce68399d1ab9d45e7ca50010882
harkl <3zy3@protonmail.ch>
2023-01-11 16:41:45 +1300

    add wallet connect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,23 @@
 import { FC } from "react";
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Box, Container, Toolbar } from "@mui/material";
 
 import DarkModeToggle from "./DarkModeToggle";
+import { WalletMultiButton } from "@solana/wallet-adapter-material-ui";
 
 export const Header: FC = () => {
   return (
-    <AppBar position="fixed" enableColorOnDark>
+    <AppBar position="fixed">
       <Toolbar variant="dense">
-        <DarkModeToggle />
+        <Container maxWidth="lg" disableGutters={true}>
+          <Box display="flex">
+            <Box sx={{ flexGrow: 1 }}>
+              <DarkModeToggle />
+            </Box>
+            <Box py={1}>
+              <WalletMultiButton />
+            </Box>
+          </Box>
+        </Container>
       </Toolbar>
     </AppBar>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 // import reportWebVitals from './reportWebVitals';
 import App from "./App";
 import ReactDOM from "react-dom/client";
-import {ChosenThemeProvider, ThemeProvider} from "./contexts";
-import {StrictMode} from "react";
+import { ChosenThemeProvider, ThemeProvider } from "./contexts";
+import { StrictMode } from "react";
+import { WalletContext } from "./contexts";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <StrictMode>
     <ChosenThemeProvider>
       <ThemeProvider>
-        <App />
+        <WalletContext>
+          <App />
+        </WalletContext>
       </ThemeProvider>
     </ChosenThemeProvider>
   </StrictMode>,
